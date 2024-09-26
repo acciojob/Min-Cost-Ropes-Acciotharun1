650e0d2d484438b19fe5ba38223d4c41e953fbcf
Acciotharun1 <166792506+Acciotharun1@users.noreply.github.com>
2024-09-26 19:27:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 function mincost(arr)
 { 
-//write your code here
-// return the min cost
+arr.sort((a, b) => {
+    return a - b;
+  });
+  let totalCost = 0;
+  while (arr.length > 1) {
+    let cost = arr.shift() + arr.shift();
+    // console.log(cost);
+    totalCost += cost;
+    arr.push(cost);
+    arr.sort((a, b) => {
+      return a - b;
+    });
+  }
+  return totalCost;
   
 }
 
